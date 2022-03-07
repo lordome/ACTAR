ab9013388863f765e1c510ce2e02ac879131c038
lordome <lorenzo.domenichetti01@gmail.com>
2022-03-07 17:22:33 +0100

    RansacTreeFill working

--- Ransac-ACTAR/RansacClassTreeFill.cpp
+++ Ransac-ACTAR/RansacClassTreeFill.cpp
@@ -116,105 +116,112 @@ int fit(string inputFileName = "input_parameters.txt")
 
     int nent = rdr.GetEntries(false);
 
+    int it_count = -1;
+
     while (rdr.Next())
     {
 
-        cout << "\rConverting entry " << rdr.GetCurrentEntry() << " of " << nent << flush;
-
-        if ( event->getEventNumber() < 1000)
+        if (rdr.GetCurrentEntry() >= it_count)
         {
-            delete fitEvt;
-            fitEvt = new cFittedEvent<cPhysicalHit>();
-            fitEvt->setEventNumber(event->getEventNumber());
-            fitEvt->setTimestamp(event->getTimestamp());
-            fitEvt->getLines().clear();
-
-            list<cPhysicalHit> hitslist = event->getHits(); // picking up the list from the event and converting it into a vector.
-
-            cTrackerRansac<cPhysicalHit> traC;
-
-            traC.setTracksEnergyThreshold(threshold);
-            traC.setBeamEnergyThreshold(fenergy);
-            traC.setTracksWidth(width);
-            traC.setBeamWidth(fwidth);
-            traC.setLoopsNumber(loops);
-            traC.setTrackMinSize(trsize);
-            traC.setBeamMinSize(besize);
-            traC.zRescaling = zRescaling;
-
-            for (auto &h : hitslist)
-            {
-                // Take only the pad hits that are set as trackable
-                if (!h.isTrackable() || h.GetEnergy() < 0)
-                {
-                    fitEvt->getUnfittedPoints().push_back(h);
-                    continue;
-                }
-
-                // The remaining points are good to be tracked
-                traC.addPoint(h);
-            }
-
-            bool beamFittable = false;
-            bool beamOneSide = false;
-            bool beamTracks = true;
-            traC.Ransac(fenergy, besize, fwidth, beamTracks, beamOneSide, beamFittable);
-
-            bool trackFittable = true;
-            bool trackOneSide = true;
-            beamTracks = false;
-
-            traC.Ransac(threshold, trsize, width, beamTracks, trackOneSide, trackFittable);
-            // End Clustering
-
-            // Fit the lines
-            traC.fitLines();
-
-            for (auto &it_line : traC.lines)
-            {
-                fitEvt->getLines().push_back(it_line);
-            }
-
-            for (auto &it_point : traC.points)
-            {
-                fitEvt->getUnfittedPoints().push_back(it_point);
-            }
-
-            // Save the fitted lines and the unfitted points in the final event
-            cVertexFinder<cPhysicalHit> vrt;
-            vrt.setMinZ(0);
-            std::vector<double> pStart = {64., 64., 500. / (zRescaling * 2.)};
-            vrt.setParStart(pStart);
-            vrt.setMaxZ(maxZ);
-            vrt.setMaxDist(vertexWidthAcceptance);
-            vrt.findVertex(fitEvt);
-
-            fOutTree->Fill();
-
-            // for (auto &line : fitEvt->getLines())
-            // {
-            //     double energyCount = 0;
-            //     for (auto &pts : line.getPoints())
-            //     {
-            //         energyCount += pts.getEnergy();
-            //     }
-            // }
-
-            // Draw the analysed Event;
-            // cDrawEvents<cFittedEvent<cPhysicalHit>> *drawEvt =
-            //     new cDrawEvents<cFittedEvent<cPhysicalHit>>(binX, binY, binZ, maxX, maxY, maxZ);
-
-            // drawEvt->setEvent(fitEvt);
-            // drawEvt->drawComponents2D(false, 0, 0, 800, 500);
-            // drawEvt->drawColors2D(false, 0, 600, 800, 500);
-            // drawEvt->drawTracks3D(true, 800, 0, 1115, 1000);
-
-            // delete drawEvt;
+            cout << "\rConverting entry " << rdr.GetCurrentEntry() << " of " << nent << flush;
+            it_count += 100;
         }
-        else
+        // cout << "\rConverting entry " << rdr.GetCurrentEntry() << " of " << nent << flush;
+
+        // if ( event->getEventNumber() < 1000)
+        // {
+        delete fitEvt;
+        fitEvt = new cFittedEvent<cPhysicalHit>();
+        fitEvt->setEventNumber(event->getEventNumber());
+        fitEvt->setTimestamp(event->getTimestamp());
+        fitEvt->getLines().clear();
+
+        list<cPhysicalHit> hitslist = event->getHits(); // picking up the list from the event and converting it into a vector.
+
+        cTrackerRansac<cPhysicalHit> traC;
+
+        traC.setTracksEnergyThreshold(threshold);
+        traC.setBeamEnergyThreshold(fenergy);
+        traC.setTracksWidth(width);
+        traC.setBeamWidth(fwidth);
+        traC.setLoopsNumber(loops);
+        traC.setTrackMinSize(trsize);
+        traC.setBeamMinSize(besize);
+        traC.zRescaling = zRescaling;
+
+        for (auto &h : hitslist)
         {
-            continue;
+            // Take only the pad hits that are set as trackable
+            if (!h.isTrackable() || h.GetEnergy() < 0)
+            {
+                fitEvt->getUnfittedPoints().push_back(h);
+                continue;
+            }
+
+            // The remaining points are good to be tracked
+            traC.addPoint(h);
         }
+
+        bool beamFittable = false;
+        bool beamOneSide = false;
+        bool beamTracks = true;
+        traC.Ransac(fenergy, besize, fwidth, beamTracks, beamOneSide, beamFittable);
+
+        bool trackFittable = true;
+        bool trackOneSide = true;
+        beamTracks = false;
+
+        traC.Ransac(threshold, trsize, width, beamTracks, trackOneSide, trackFittable);
+        // End Clustering
+
+        // Fit the lines
+        traC.fitLines();
+
+        for (auto &it_line : traC.lines)
+        {
+            fitEvt->getLines().push_back(it_line);
+        }
+
+        for (auto &it_point : traC.points)
+        {
+            fitEvt->getUnfittedPoints().push_back(it_point);
+        }
+
+        // Save the fitted lines and the unfitted points in the final event
+        cVertexFinder<cPhysicalHit> vrt;
+        vrt.setMinZ(0);
+        std::vector<double> pStart = {64., 64., 500. / (zRescaling * 2.)};
+        vrt.setParStart(pStart);
+        vrt.setMaxZ(maxZ);
+        vrt.setMaxDist(vertexWidthAcceptance);
+        vrt.findVertex(fitEvt);
+
+        fOutTree->Fill();
+
+        // for (auto &line : fitEvt->getLines())
+        // {
+        //     double energyCount = 0;
+        //     for (auto &pts : line.getPoints())
+        //     {
+        //         energyCount += pts.getEnergy();
+        //     }
+        // }
+
+        // Draw the analysed Event;
+        // cDrawEvents<cFittedEvent<cPhysicalHit>> *drawEvt =
+        //     new cDrawEvents<cFittedEvent<cPhysicalHit>>(binX, binY, binZ, maxX, maxY, maxZ);
+
+        // drawEvt->setEvent(fitEvt);
+        // drawEvt->drawComponents2D(false, 0, 0, 800, 500);
+        // drawEvt->drawColors2D(false, 0, 600, 800, 500);
+        // drawEvt->drawTracks3D(true, 800, 0, 1115, 1000);
+
+        // delete drawEvt;
+        // }
+        // else
+        // {
+        //     continue;
+        // }
     }
 
     fOutTree->Write();
